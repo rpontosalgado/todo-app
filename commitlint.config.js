feaf499a17225e1a62de58a15aeff7f2ebbf9779
rpontosalgado <r.salgado3@gmail.com>
2026-05-20 16:23:03 -0300

    chore(commitlint): add custom validation rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"]
+  }
+};
