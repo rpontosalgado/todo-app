90c663f7f3f5ef59918abba7bd08d3ec94e12433
rpontosalgado <r.salgado3@gmail.com>
2026-05-20 17:43:22 -0300

    feat(components): add TodoItem component with edit/delete functionality

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Button, View } from 'react-native'
+import { Todo } from '../../types'
+import * as S from './TodoItem.styles'
+
+interface TodoItemProps {
+  todo: Todo
+  onEdit: (id: string, name: string) => void
+  onDelete: (id: string) => void
+}
+
+const TodoItem = ({ todo, onEdit, onDelete }: TodoItemProps) => {
+  const [isEditing, setIsEditing] = React.useState(false)
+  const [editName, setEditName] = React.useState(todo.name)
+
+  const handleSave = () => {
+    if (editName.trim()) {
+      onEdit(todo.id, editName)
+      setIsEditing(false)
+    }
+  }
+
+  const handleCancel = () => {
+    setEditName(todo.name)
+    setIsEditing(false)
+  }
+
+  if (isEditing) {
+    return (
+      <S.Container>
+        <S.Label>Editar:</S.Label>
+        <S.Input
+          value={editName}
+          onChangeText={setEditName}
+          onBlur={handleSave}
+          autoFocus
+        />
+        <S.ButtonContainer>
+          <Button title="Salvar" onPress={handleSave} />
+          <Button title="Cancelar" onPress={handleCancel} color="#999" />
+        </S.ButtonContainer>
+      </S.Container>
+    )
+  }
+
+  return (
+    <S.Container>
+      <S.Header>
+        <View>
+          <S.Name>{todo.name}</S.Name>
+          <S.Date>
+            {new Date(todo.createdAt).toLocaleDateString('pt-BR')}
+          </S.Date>
+        </View>
+      </S.Header>
+      <S.Buttons>
+        <Button title="Editar" onPress={() => setIsEditing(true)} />
+        <Button
+          title="Remover"
+          onPress={() => onDelete(todo.id)}
+          color="#f44336"
+        />
+      </S.Buttons>
+    </S.Container>
+  )
+}
+
+export { TodoItem }
