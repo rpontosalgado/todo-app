f5f7b940409d68f4dee87a14bd723c75933614be
rpontosalgado <r.salgado3@gmail.com>
2026-05-20 17:43:30 -0300

    feat(components): add TodoList component with FlatList

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState } from 'react'
+import { Button, FlatList, TextInput } from 'react-native'
+import { TodoContext } from '../../contexts/TodoContext'
+import { TodoItem } from '../TodoItem/TodoItem'
+import * as S from './TodoList.styles'
+
+const TodoList = () => {
+  const context = useContext(TodoContext)
+  if (!context) {
+    return null
+  }
+  const { todos, updateTodo, deleteTodo, addTodo } = context
+  const [newTodo, setNewTodo] = useState('')
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      addTodo(newTodo)
+      setNewTodo('')
+    }
+  }
+
+  return (
+    <S.Container>
+      <S.AddContainer>
+        <S.Title>To-Do List</S.Title>
+        <S.Input
+          placeholder="Adicionar novo item..."
+          value={newTodo}
+          onChangeText={setNewTodo}
+          onSubmitEditing={handleAddTodo}
+        />
+        <Button title="Adicionar" onPress={handleAddTodo} />
+      </S.AddContainer>
+      <FlatList
+        data={todos}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <TodoItem todo={item} onEdit={updateTodo} onDelete={deleteTodo} />
+        )}
+        contentContainerStyle={{
+          paddingBottom: 16
+        }}
+      />
+    </S.Container>
+  )
+}
+
+export { TodoList }
