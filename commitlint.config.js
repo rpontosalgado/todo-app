e2b14b31fc540b7b9f564714bdb964bf06e795ea
rpontosalgado <r.salgado3@gmail.com>
2026-05-22 03:18:32 -0300

    fix: use correct commitizen hook with exec for stdin passthrough

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-  },
+  extends: ['@commitlint/config-conventional']
 };
