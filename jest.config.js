15323c808a96096a95f282037efd69a1e42d6593
rpontosalgado <r.salgado3@gmail.com>
2026-05-21 23:27:16 -0300

    chore: configure Jest for React Native component testing
    Switch to react-native preset, add transformIgnorePatterns for Expo
    deps, install @testing-library/react-native and react-test-renderer

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
+  preset: 'react-native',
   verbose: true,
   testPathIgnorePatterns: ['/node_modules/', '/expo/'],
   collectCoverageFrom: ['src/**/*.ts{,x}'],
   coverageDirectory: 'coverage',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-paper|react-native-safe-area-context|@react-native-async-storage|styled-components|expo|expo-status-bar|@expo)/)',
+  ],
 };
